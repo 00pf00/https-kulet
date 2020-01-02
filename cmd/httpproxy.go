fad283d6d55523070a4dbede11d71642d39715a6
00pf00 <tengfei_2017@sina.com>
2020-01-02 18:37:38 +0800

    websocket

--- cmd/httpproxy.go
+++ cmd/httpproxy.go
@@ -2,13 +2,9 @@ package main
 
 import (
 	"00pf00/https-kulet/pkg/https/client"
-	"fmt"
-	"os"
 )
 
 func main() {
-	dir,_ := os.Getwd()
-	fmt.Printf("workpath = %v\n",dir)
 	httpclient := client.NewClient()
 	httpclient.Post()
 }
