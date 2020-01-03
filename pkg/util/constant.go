87c7f8c23f74137c18bbb75b9e6e3d0b5fd1d803
feeli <feeli@tencent.com>
2020-01-03 23:20:24 +0800

    wss

--- pkg/util/constant.go
+++ pkg/util/constant.go
@@ -2,7 +2,10 @@ package util
 
 //client
 const (
-	CLIENT_CERT = ".\\conf\\cert\\kubelet.crt"
-	CLIENT_KEY  = ".\\conf\\cert\\kubelet.key"
-	SERVER_ADDR = "https://10.112.217.199:10250/exec/default/account-dlxcz/account?command=ls&error=1&output=1"
+	//CLIENT_CERT = "./conf/cert/kube-apiserver-kubelet-client-cert.crt"
+	//CLIENT_KEY  = "./conf/cert/kube-apiserver-kubelet-client-key.key"
+	//SERVER_ADDR = "https://kubernetes-master:10250/exec/default/ng-0/web-server?command=touch&command=ls&input=1&output=1&tty=1"
+	CLIENT_CERT = "./conf/cert/apiserver-kubelet-client.crt"
+	CLIENT_KEY  = "./conf/cert/apiserver-kubelet-client.key"
+	SERVER_ADDR = "https://49.51.38.39:10250/exec/default/ng-0/web-server?command=touch&command=ls&input=1&output=1&tty=1"
 )
