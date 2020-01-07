af1b86b2314cc93a6f0c1a03556dcf369b252528
feeli <feeli@tencent.com>
2020-01-07 17:44:26 +0800

    redirect

--- pkg/util/constant.go
+++ pkg/util/constant.go
@@ -2,13 +2,16 @@ package util
 
 //client
 const (
-	//CLIENT_CERT = "./conf/cert/kube-apiserver-kubelet-client-cert.crt"
-	//CLIENT_KEY  = "./conf/cert/kube-apiserver-kubelet-client-key.key"
+	//CLIENT_CERT = "./conf/cert/client/kube-apiserver-kubelet-client-cert.crt"
+	//CLIENT_KEY  = "./conf/cert/client/kube-apiserver-kubelet-client-key.key"
 	//SERVER_ADDR = "https://kubernetes-master:10250/exec/default/ng-0/web-server?command=touch&command=ls&input=1&output=1&tty=1"
-	//CLIENT_CERT = "./conf/cert/apiserver-kubelet-client.crt"
-	//CLIENT_KEY  = "./conf/cert/apiserver-kubelet-client.key"
-	//SERVER_ADDR = "https://49.51.38.39:10250/exec/default/ng-0/web-server?command=touch&command=ls&input=1&output=1&tty=1"
-	CLIENT_CERT = "./conf/cert/kubelet.crt"
-	CLIENT_KEY  = "./conf/cert/kubelet.key"
-	SERVER_ADDR = "https://192.168.1.24:10250/exec/default/account-ccnpc/account?command=touch&command=ls&input=1&output=1&tty=1"
+	CLIENT_CERT = "./conf/cert/client/apiserver-kubelet-client.crt"
+	CLIENT_KEY  = "./conf/cert/client/apiserver-kubelet-client.key"
+	SERVER_ADDR = "https://49.51.38.39:10250/exec/default/ng-0/web-server?command=touch&command=ls&input=1&output=1&tty=1"
+	//SERVER_ADDR = "https://127.0.0.1:10250/exec"
+	SERVER_CERT = "./conf/cert/server/kubelet.crt"
+	SERVER_KEY  = "./conf/cert/server/kubelet.key"
+	//CLIENT_CERT = "./conf/cert/client/kubelet.crt"
+	//CLIENT_KEY  = "./conf/cert/client/kubelet.key"
+	//SERVER_ADDR = "https://192.168.1.24:10250/exec/default/account-ccnpc/account?command=touch&command=ls&input=1&output=1&tty=1"
 )
