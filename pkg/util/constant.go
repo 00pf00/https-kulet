24a5fcd7e3489539d346cff6125019021087506b
feeli <feeli@tencent.com>
2020-01-07 11:59:48 +0800

    hwcloud

--- pkg/util/constant.go
+++ pkg/util/constant.go
@@ -2,10 +2,13 @@ package util
 
 //client
 const (
-	CLIENT_CERT = "./conf/cert/kube-apiserver-kubelet-client-cert.crt"
-	CLIENT_KEY  = "./conf/cert/kube-apiserver-kubelet-client-key.key"
-	SERVER_ADDR = "https://kubernetes-master:10250/exec/default/ng-0/web-server?command=touch&command=ls&input=1&output=1&tty=1"
+	//CLIENT_CERT = "./conf/cert/kube-apiserver-kubelet-client-cert.crt"
+	//CLIENT_KEY  = "./conf/cert/kube-apiserver-kubelet-client-key.key"
+	//SERVER_ADDR = "https://kubernetes-master:10250/exec/default/ng-0/web-server?command=touch&command=ls&input=1&output=1&tty=1"
 	//CLIENT_CERT = "./conf/cert/apiserver-kubelet-client.crt"
 	//CLIENT_KEY  = "./conf/cert/apiserver-kubelet-client.key"
 	//SERVER_ADDR = "https://49.51.38.39:10250/exec/default/ng-0/web-server?command=touch&command=ls&input=1&output=1&tty=1"
+	CLIENT_CERT = "./conf/cert/kubelet.crt"
+	CLIENT_KEY  = "./conf/cert/kubelet.key"
+	SERVER_ADDR = "https://49.51.38.39:10250/exec/default/account-ccnpc/account?command=touch&command=ls&input=1&output=1&tty=1"
 )
